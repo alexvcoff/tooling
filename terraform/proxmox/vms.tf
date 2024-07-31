ec43dad3d85a153420ff80fafc01a11c98e921f4
Aleksandr Semenov <bloodex.drop@gmail.com>
2024-07-31 15:49:06 +0300

    Update VM config

--- terraform/proxmox/vms.tf
+++ terraform/proxmox/vms.tf
@@ -1,14 +1,15 @@
 resource "proxmox_vm_qemu" "k8s-master" {
   name        = "kube-master1"
   target_node = "proxmox"
-  vmid        = "101"
+  vmid        = 101
   clone       = "template-ubuntu-24-04"
-#   os_type     = "cloud-init"
+  on_boot     = true
+  os_type     = "cloud-init"
 #   cpu         = "kvm64"
   cores       = 2
   sockets     = 1
   memory      = 2048
-#   scsihw      = "virtio-scsi-pci"
+  scsihw      = "virtio-scsi-pci"
 #   bootdisk    = "scsi0"
 
   network {
@@ -17,4 +18,6 @@ resource "proxmox_vm_qemu" "k8s-master" {
     firewall  = false
     link_down = false
   }
+
+  ipconfig0 = "ip=192.168.1.201/24,gw=192.168.11.1"
 }
