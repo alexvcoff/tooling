02021ab1f811bd0cf733e5e240ff13800b02a9de
Aleksandr Semenov <bloodex.drop@gmail.com>
2024-08-06 15:45:54 +0300

    Update providers

--- terraform/proxmox/providers.tf
+++ terraform/proxmox/providers.tf
@@ -4,10 +4,6 @@ terraform {
       source  = "telmate/proxmox"
       version = ">=3.0.1-rc3"
     }
-    # proxmox = {
-    #   source = "bpg/proxmox"
-    #   version = "0.61.1"
-    # }
   }
 }
 
@@ -16,4 +12,7 @@ provider "proxmox" {
   pm_api_token_id = var.PROXMOX_API_TOKEN
   pm_api_token_secret = var.PROXMOX_API_SECRET
   pm_tls_insecure     = true
+#   endpoint = "https://${var.PROXMOX_HOST}/api2/json"
+#   api_token = "${var.PROXMOX_API_TOKEN}=${var.PROXMOX_API_SECRET}"
+#   insecure  = true
 }
